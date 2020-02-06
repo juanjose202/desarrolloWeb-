961298fb3a0e498a2c6b0c8de35637dc379d30a5
juanjose202 <juanjosegomez1908@gmail.com>
2020-02-06 07:36:48 -0500

    javascript

---
+++ Frontend/EjercicioPaginaIngenieria_html/intro-js/index.js
@@ -0,0 +1,31 @@
+//console  comentario en linea
+
+/**
+ * Comentario en bloque
+ */
+
+console.log("Hola Mundo")
+console.info("info")
+console.error("error")
+console.warn("Alerta")
+
+let nombre = "juan jose"
+let apellido = "gomez granada"
+const edad = 50
+var color = "Blanco"
+
+let altura
+let esta_vivo = true
+
+let nombreCompleto1 = nombre + " " + apellido
+
+console.log(nombreCompleto1)
+console.log(``, )
+
+if (edad > 49) {
+    let nombre = "otro nombre"
+    var color = "otro color"
+    console.log(color);
+}
+
+console.log(nombre, color)
